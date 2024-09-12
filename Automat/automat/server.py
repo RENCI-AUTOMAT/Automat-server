e7d55a67e2c890e253ac6ece5de9f39e8c381303
Evan Morris <evandietzmorris@gmail.com>
2024-09-12 11:19:10 -0400

    switching from os environ to config

--- Automat/automat/server.py
+++ Automat/automat/server.py
@@ -1,24 +1,24 @@
-import os
+from automat.config import config
 from automat.core import Automat
 from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
 
 automat = Automat()
 
-if os.environ.get("OTEL_ENABLED", "False").lower() == "true":
+if config.get("OTEL_ENABLED", "False").lower() == "true":
     from opentelemetry import trace
     from opentelemetry.sdk.resources import SERVICE_NAME, Resource
     from opentelemetry.sdk.trace import TracerProvider
     from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
     resource = Resource(attributes={
-        SERVICE_NAME: os.environ.get("OTEL_SERVICE_NAME", "Automat"),
+        SERVICE_NAME: config.get("OTEL_SERVICE_NAME", "Automat"),
     })
     provider = TracerProvider(resource=resource)
 
-    otel_host = os.environ.get("OTEL_COLLECTOR_HOST", None)
+    otel_host = config.get("OTEL_COLLECTOR_HOST", None)
     if otel_host:
         from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-        otel_port = os.environ.get("OTEL_COLLECTOR_PORT", "4317")
+        otel_port = config.get("OTEL_COLLECTOR_PORT", "4317")
         otel_endpoint = f'{otel_host}:{otel_port}'
         otel_exporter = OTLPSpanExporter(endpoint=f'{otel_endpoint}')
         processor = BatchSpanProcessor(otel_exporter)
